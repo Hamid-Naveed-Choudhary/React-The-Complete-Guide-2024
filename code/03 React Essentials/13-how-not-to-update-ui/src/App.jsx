5c04085836b40928bd69ae81f830cc7d46847ca7
Hamid Naveed <hamidnaveedch@gmail.com>
2024-11-01 00:40:31 +0500

    53. How NOT to Update the UI - A Look Behind The Scenes of React [Core Concept]

---
+++ code/03 React Essentials/13-how-not-to-update-ui/src/App.jsx
@@ -0,0 +1,50 @@
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  let tabContent = "Please click a button";
+
+  function handleSelect(selectedButton) {
+    tabContent = selectedButton;
+    console.log(tabContent);
+  }
+
+  console.log("APP COMPONENT EXECUTING");
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
