4c1a92aee464ceb5771d4afb1fdcbfafb5c2ad90
Hamid Naveed <hamidnaveedch@gmail.com>
2024-11-18 17:40:06 +0500

    Setting Component Types Dynamically and Setting Default Prop Values

---
+++ code/04 Essentials Deep Dive/06-dynamic-component-types/src/components/Examples.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import TabButton from "./TabButton.jsx";
+import Section from "./Section.jsx";
+import Tabs from "./Tabs.jsx";
+import { EXAMPLES } from "../data.js";
+
+export default function Exaples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    //console.log(selectedTopic);
+  }
+
+  return (
+    <Section title="Examples" id="examples">
+      <Tabs
+        // buttonsContainer="menu"
+        buttons={
+          <>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onClick={() => handleSelect("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onClick={() => handleSelect("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onClick={() => handleSelect("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onClick={() => handleSelect("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {!selectedTopic ? <p>Please select a topic.</p> : null}
+        {selectedTopic ? (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+        ) : null}
+      </Tabs>
+    </Section>
+  );
+}
