65e3e17fa08540ba01e2b286579e8562893637b3
Hamid Naveed <hamidnaveedch@gmail.com>
2024-11-18 17:00:13 +0500

    Working with Multiple JSX Slots

---
+++ code/04 Essentials Deep Dive/05-multiple-jsx-slots/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Exaples from "./components/Examples.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Exaples />
+      </main>
+    </>
+  );
+}
+
+export default App;
